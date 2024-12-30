e57d120076d57869090eb8a6875c816cfcfcc47d
Larissa Schaefer <larito@gmx.de>
2024-12-30 23:14:35 +0100

    try without api

--- app/client.py
+++ app/client.py
@@ -1,11 +1,23 @@
 import streamlit as st
 from PIL import Image
-import requests
+from models.text import load_multi_modal_model, generate_image_caption
+# import requests
 
 st.title("Image Caption Generator")
 
+
+# Caching the model loading
+@st.cache_resource
+def get_model():
+    with st.spinner("Loading model..."):
+        processor, model = load_multi_modal_model()
+        return processor, model
+
+
+processor, model = get_model()
+
 # Configurable API URL
-api_url = st.secrets.get("API_URL", "http://localhost:8000/generate/caption")
+# api_url = st.secrets.get("API_URL", "http://localhost:8000/generate/caption")
 
 uploaded_file = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
 
@@ -18,13 +30,20 @@ if uploaded_file is not None:
     else:
         if st.button("Generate Image Caption"):
             with st.spinner("Generating caption..."):
-                files = {"image": uploaded_file.getvalue()}  # Send image data
                 try:
-                    response = requests.post(api_url, files=files)
-                    response.raise_for_status()
-                except requests.exceptions.RequestException as e:
-                    st.error(f"Failed to generate caption: {e}")
-                else:
-                    result = response.json()
-                    caption = result.get("caption", "No caption received")
+                    caption = generate_image_caption(processor, model, image)
                     st.success(f"Generated Caption: '{caption}'")
+                except Exception as e:
+                    st.error(f"Failed to generate caption: {e}")
+        # if st.button("Generate Image Caption"):
+        #     with st.spinner("Generating caption..."):
+        #         files = {"image": uploaded_file.getvalue()}  # Send image data
+        #         try:
+        #             response = requests.post(api_url, files=files)
+        #             response.raise_for_status()
+        #         except requests.exceptions.RequestException as e:
+        #             st.error(f"Failed to generate caption: {e}")
+        #         else:
+        #             result = response.json()
+        #             caption = result.get("caption", "No caption received")
+        #             st.success(f"Generated Caption: '{caption}'")
